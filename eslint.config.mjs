9067775b2387ce5eb66be8195462b18ffae7f2da
Mohammed <msshaikh10@myseneca.ca>
2025-09-22 15:23:40 -0400

    Commiting changes before deleting src/auth.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
