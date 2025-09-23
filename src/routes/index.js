e674b7a639e3de7b72d8565ab71b07250b0c408c
Mohammed <msshaikh10@myseneca.ca>
2025-09-22 22:13:24 -0400

    Final Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,9 @@ const router = express.Router();
 // Our authentication middleware
 const { authenticate } = require('../auth');
 
+//HTTP response update to use createSuccessResponse
+const { createSuccessResponse } = require('../response');
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  * Protect them all with middleware so you have to be authenticated
@@ -27,13 +30,11 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
+  res.status(200).json(createSuccessResponse({
     author,
-    // Use your own GitHub URL for this!
     githubUrl: 'https://github.com/MohammedSShaikh/fragments',
     version,
-  });
+  }));
 });
 
 module.exports = router;
