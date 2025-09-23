ad7a308f11a2515e456e0e2296704cbfab031b1c
Mohammed <msshaikh10@myseneca.ca>
2025-09-22 21:45:33 -0400

    break test and esLint

--- test/unit/get.test.js
+++ test/unit/get.test.js
@@ -3,6 +3,7 @@
 const request = require('supertest');
 
 const app = require('../../src/app');
+const unNeeded = 41
 
 describe('GET /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
